9a3d703124d11e701eebad1060377c0cc7b96866
tijnsemmekrot <tsemmekrot@gmail.com>
2026-03-30 15:52:22 +0200

    kargo files

--- build-images.sh
+++ build-images.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-
+# build-images.sh
 DOCKER_USER="tijnsemmekrot"
 REPO_NAME="traces-test"
+VERSION=$(date +%Y%m%d-%H%M)
 
-for service in account-service fraud-detector notification-service payment-processor payment-requester; do
-  echo "Building and Pushing $service..."
-  docker build -t "$DOCKER_USER/$REPO_NAME:$service" "./apps/$service"
-  docker push "$DOCKER_USER/$REPO_NAME:$service"
+SERVICES=("account-service" "fraud-detector" "notification-service" "payment-processor" "payment-requester")
+
+for service in "${SERVICES[@]}"; do
+  TAG="$service-$VERSION"
+  echo "--- Building $service with tag $TAG ---"
+  docker build -t "$DOCKER_USER/$REPO_NAME:$TAG" "./apps/$service"
+  docker push "$DOCKER_USER/$REPO_NAME:$TAG"
 done
