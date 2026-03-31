df4862176d9cf34fc48454de4dcddefb40f02586
tijnsemmekrot <tsemmekrot@gmail.com>
2026-03-31 09:47:43 +0200

    updated kargo repo, with seperate docker repo's

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # build-images.sh
 DOCKER_USER="tijnsemmekrot"
-REPO_NAME="traces-test"
 VERSION=$(date +%Y%m%d-%H%M)
 
 # Determine the directory where the script is located
@@ -10,10 +9,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SERVICES=("account-service" "fraud-detector" "notification-service" "payment-processor" "payment-requester")
 
 for service in "${SERVICES[@]}"; do
-  TAG="$service-$VERSION"
+  REPO_NAME="traces-test-$service"
+  TAG="$VERSION"
   echo "--- Building $service with tag $TAG ---"
 
   # Use the absolute path relative to the script location
   docker build -t "$DOCKER_USER/$REPO_NAME:$TAG" "$SCRIPT_DIR/apps/$service"
   docker push "$DOCKER_USER/$REPO_NAME:$TAG"
 done
+
+echo "All images built and pushed successfully!"
